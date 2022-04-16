36a6e3639b9c0faf6928e06e89a97c10e76d8ae8
hungrytech <xorals9448@gmail.com>
2022-04-16 20:25:09 +0900

    init commit

---
+++ src/main/java/com/teak/cicdtestapp/CiciTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.teak.cicdtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiciTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiciTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teak/cicdtestapp/CiciTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teak.cicdtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiciTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
