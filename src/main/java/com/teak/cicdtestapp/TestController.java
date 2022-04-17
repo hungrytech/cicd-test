125f2c149e9e23d49002e0f8d6838bae9e8f9d50
hungrytech <xorals9448@gmail.com>
2022-04-17 17:04:40 +0900

    테스트 API 수저 (배포 test)

--- src/main/java/com/teak/cicdtestapp/TestController.java
+++ src/main/java/com/teak/cicdtestapp/TestController.java
@@ -14,6 +14,6 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<String> test2() {
-        return ResponseEntity.ok("테스트 API slack message");
+        return ResponseEntity.ok("테스트 API slack message slack");
     }
 }
